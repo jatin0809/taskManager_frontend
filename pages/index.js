2de32f3b4be5d379bed117b4298e74cfe5062b8e
jatin0809 <129044916+jatin0809@users.noreply.github.com>
2024-10-23 16:31:45 +0530

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Analytics from "./analytics";
+import Dashboard from "./dashboard";
+import Login from "./login";
+import Register from "./register";
+import Settings from "./settings";
+import Share from "./share";
+import NotFound from "./notFound";
+
+
+export {
+    Analytics,
+    Dashboard,
+    Login,
+    Register,
+    Settings,
+    Share,
+    NotFound
+}
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Analytics, Dashboard, Login, Register, Settings, Share, NotFound } from "../pages";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard/>}  />
+        <Route path="/dashboard" element={<Dashboard/>}  />
+        <Route path="/analytics" element={<Analytics/>}  />
+        <Route path="/login" element={<Login/>}  />
+        <Route path="register/" element={<Register/>}  />
+        <Route path="/settings" element={<Settings/>}  />
+        <Route path="/share" element={<Share/>}  />
+        <Route path="*" element={<NotFound/>}  />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
